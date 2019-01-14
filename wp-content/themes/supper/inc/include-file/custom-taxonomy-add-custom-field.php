16e3bcdfadca7b54f34176f32268ac8287f8b69d
thongwav <luongthongfn@gmail.com>
2019-01-14 09:54:18 +0700

    pause dang lam upload media (inc/include-file/custom-taxonomy-add-field)

--- wp-content/themes/supper/inc/include-file/custom-taxonomy-add-custom-field.php
+++ wp-content/themes/supper/inc/include-file/custom-taxonomy-add-custom-field.php
@@ -1,7 +1,11 @@
 <?php
 // thêm trang term
+
 function pippin_taxonomy_add_new_meta_field()
 {
+    //loadscript of media upload
+    add_action('admin_enqueue_scripts', "wp_enqueue_media");
+
     // thêm custom meta field vào trang add new term
     ?>
         <div>
@@ -24,42 +28,47 @@ add_action('prod_cate_add_form_fields', 'pippin_taxonomy_add_new_meta_field', 10
 // chỉnh sửa trang term
 function pippin_taxonomy_edit_meta_field($term)
 {
+    //loadscript of media upload
+    add_action('admin_enqueue_scripts', "wp_enqueue_media");
+
     // gán term ID vào một biến
     $t_id = $term->term_id;
 
     // truy xuất giá trị hiện tại cho meta field này, trả về một mảng
     $term_meta = get_option("taxonomy_$t_id");?>
-        <tr>
-            <th scope="row" valign="top">
-                <label for="term_meta[custom_term_meta]">
-                    <?php _e('Ảnh danh mục', 'super');?>
-                </label>
-            </th>
-            <td>
-                <input
-                    type="file"
+        <tbody>
+            <tr>
+                <th scope="row" valign="top">
+                    <label for="term_meta[custom_term_meta]">
+                        <?php _e('Ảnh danh mục', 'super');?>
+                    </label>
+                </th>
+                <td>
+                    <input
+                        type="file"
 
-                    id="term_meta[custom_term_meta]"
-                    name="term_meta[custom_term_meta]"
-                    value="<?php echo esc_attr($term_meta['custom_term_meta']) ? esc_attr($term_meta['custom_term_meta']) : ''; ?>">
-                <p><?php _e('Enter a value for this field', 'super');?></p>
-            </td>
-        </tr>
-        <tr>
-            <th scope="row" valign="top">
-                <label for="term_meta[term_feature_icon]">
-                    <?php _e('Icon danh mục', 'super');?>
-                </label>
-            </th>
-            <td>
-                <input
-                    type="file"
-                    id="term_meta[term_feature_icon]"
-                    name="term_meta[term_feature_icon]"
-                    value="<?php echo esc_attr($term_meta['term_feature_icon']) ? esc_attr($term_meta['term_feature_icon']) : ''; ?>">
-                <p><?php _e('Enter a value for this field', 'super');?></p>
-            </td>
-        </tr>
+                        id="term_meta[custom_term_meta]"
+                        name="term_meta[custom_term_meta]"
+                        value="<?php echo esc_attr($term_meta['custom_term_meta']) ? esc_attr($term_meta['custom_term_meta']) : ''; ?>">
+                    <p><?php _e('Enter a value for this field', 'super');?></p>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" valign="top">
+                    <label for="term_meta[term_feature_icon]">
+                        <?php _e('Icon danh mục', 'super');?>
+                    </label>
+                </th>
+                <td>
+                    <input
+                        type="file"
+                        id="term_meta[term_feature_icon]"
+                        name="term_meta[term_feature_icon]"
+                        value="<?php echo esc_attr($term_meta['term_feature_icon']) ? esc_attr($term_meta['term_feature_icon']) : ''; ?>">
+                    <p><?php _e('Enter a value for this field', 'super');?></p>
+                </td>
+            </tr>
+        </tbody>
 <?php
 }
 add_action('prod_cate_edit_form_fields', 'pippin_taxonomy_edit_meta_field', 10, 2);
--- wp-content/themes/supper/inc/include-file/style-script-register.php
+++ wp-content/themes/supper/inc/include-file/style-script-register.php
@@ -41,3 +41,10 @@ function add_post_validtate()
     wp_enqueue_script('my_script_js', get_template_directory_uri() . '/assets/js/adminPost.validation.js');
 }
 add_action('admin_enqueue_scripts', 'add_post_validtate');
+
+//upload media
+// function admin_upload_media()
+// {
+//     wp_enqueue_media();
+// }
+// add_action('admin_enqueue_scripts', 'admin_upload_media');
